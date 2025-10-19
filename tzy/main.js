ae0fe0c92575c02939202bc3c88d99edcdd22574
Leslie-1900 <2687753143@qq.com>
2025-10-19 20:51:38 +0800

    TangZiyu251203

---
+++ tzy/main.js
@@ -0,0 +1,34 @@
+const { createApp, ref, computed, reactive, toRefs } = Vue
+
+const app = createApp({
+    setup() {
+        const cart = ref([])
+        const premium = ref(true)
+        
+        function updateCart(id) {
+            cart.value.push(id)
+        }
+        
+        function removeFromCart(id) {
+            const index = cart.value.indexOf(id)
+            if (index > -1) {
+                cart.value.splice(index, 1)
+            }
+        }
+        
+        return {
+            cart,
+            premium,
+            updateCart,
+            removeFromCart
+        }
+    }
+})
+
+// 注册所有组件
+app.component('product-display', productDisplay)
+app.component('product-details', productDetails)
+app.component('review-form', reviewForm)
+app.component('review-list', reviewList)
+
+app.mount('#app')
