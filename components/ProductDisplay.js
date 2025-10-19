0bfc023cf1bf24878387a9b5b0fb3eae021a2f6a
Leslie-1900 <2687753143@qq.com>
2025-10-19 20:57:15 +0800

    Add files via upload

---
+++ components/ProductDisplay.js
@@ -0,0 +1,123 @@
+const productDisplay = {
+    template: /*html*/ `
+    <div class="product-display">
+        <div class="product-container">
+            <div class="product-image">
+                <img :src="image">
+            </div>
+        </div>
+        <div class="product-info">
+            <h1>{{title}}</h1>
+            <p v-if="inventory > 10">In Stock</p>
+            <p v-else-if="inventory <= 10 && inventory > 0">Almost out of Stock</p>
+            <p v-else>Out of Stock</p>
+            <p>Shipping: {{shipping}}</p>
+            
+            <product-details :details="details"></product-details>
+            
+            <div v-for="(variant,index) in variants" :key="variant.id" @mouseover="updateVariant(index)"
+                class="color-circle" :style="{backgroundColor: variant.color}">
+            </div>
+            <button class="button" :disabled='!inStock' @click="addToCart" :class="{disabledButton: !inStock}">
+                Add To Cart
+            </button>
+            <button class="button remove-from-cart" @click="removeFromCart">
+                Remove From Cart
+            </button>
+        </div>
+        
+        <review-list v-if="reviews.length" :reviews="reviews"></review-list>
+        <review-form @review-submitted="addReview"></review-form>
+    </div>
+    `,
+    props: {
+        premium: Boolean
+    },
+    setup(props, { emit }) {
+        const product = ref('Boots')
+        const brand = ref('SE 331')
+        const inventory = ref(100)
+        const details = ref([
+            '50% cotton',
+            '30% wool',
+            '20% polyester'
+        ])
+        const variants = ref([
+            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
+        ])
+        const selectedVariant = ref(0)
+        const reviews = ref([])
+        
+        const shipping = computed(() => {
+            if (props.premium) {
+                return 'Free'
+            } else {
+                return '30'
+            }
+        })
+        
+        function updateVariant(index) {
+            selectedVariant.value = index;
+        }
+        
+        const image = computed(() => {
+            return variants.value[selectedVariant.value].image
+        })
+        
+        const inStock = computed(() => {
+            return variants.value[selectedVariant.value].quantity > 0
+        })
+        
+        function addToCart() {
+            emit('add-to-cart', variants.value[selectedVariant.value].id)
+        }
+        
+        function removeFromCart() {
+            emit('remove-from-cart', variants.value[selectedVariant.value].id)
+        }
+        
+        function addReview(review) {
+            reviews.value.push(review)
+        }
+        
+        const title = computed(() => {
+            return brand.value + ' ' + product.value
+        })
+        
+        return {
+            title,
+            image,
+            inStock,
+            inventory,
+            details,
+            variants,
+            reviews,
+            addToCart,
+            removeFromCart,
+            addReview,
+            updateVariant,
+            shipping
+        }
+    }
+}
+
+// product-details 组件
+const productDetails = {
+    template: /*html*/ `
+    <div class="product-details">
+        <ul class="details-list">
+            <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+    </div>
+    `,
+    props: {
+        details: {
+            type: Array,
+            required: true
+        }
+    },
+    setup(props) {
+        return {}
+    }
+}
